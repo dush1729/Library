2ffa010d792d5bdc08ced5d76b3482206ef4b19e
Ta180m <ta180m@gmail.com>
2019-08-02 22:03:09 -0500

    Update sparse_table.cpp

--- Data Structures/sparse_table.cpp
+++ Data Structures/sparse_table.cpp
@@ -6,9 +6,9 @@ public:
 	sparse_table(vector<T> &A) {
 		int N = A.size();
 		for (int i = 0; i <= log(N); i++) st[i].resize(N);
-		for (int i = 0; i < N; i++) st[i][0] = A[i];
-		for (int i = 0; i <= log(N); i++) {
-			for (int j = 1; j + (1 << i) < N; j++) st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+		for (int i = 0; i < N; i++) st[0][i] = A[i];
+		for (int i = 1; i <= log(N); i++) {
+			for (int j = 0; j + (1 << i) < N; j++) st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
 		}
 	}
 
