585ac8606d0407362ec5dfb52865d014963a511c
Ta180m <ta180m@gmail.com>
2019-08-06 14:35:23 -0500

    Update util.cpp

--- Miscellaneous/util.cpp
+++ Miscellaneous/util.cpp
@@ -1,7 +1,7 @@
 template <class BidirectionalIterator> // Coordinate compression
-void compress (BidirectionalIterator first, BidirectionalIterator last) {
-	vector<int> tmp(first, last);
+void compress(BidirectionalIterator first, BidirectionalIterator last) {
+	vector<auto> tmp(first, last);
 	sort(tmp.begin(), tmp.end());
 	tmp.resize(unique(tmp.begin(), tmp.end()) - tmp.begin());
-	for (auto it = first; it != last; it++) *it = lower_bound(tmp.begin(), tmp.end(), *it) - tmp.begin();
+	for (auto it = first; it != last; it++)* it = lower_bound(tmp.begin(), tmp.end(), *it) - tmp.begin();
 }
