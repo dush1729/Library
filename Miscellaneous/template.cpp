913348c9d2b3b0edb2c0c64f03ac31f3822e69e4
Ta180m <ta180m@gmail.com>
2019-08-13 20:20:12 -0500

    Create template.cpp

---
+++ Miscellaneous/template.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <cmath>
+#include <cstring>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+constexpr auto INF = (int)1e9;
+
+int main() {
+	ifstream cin(".in");
+    ofstream cout(".out");
+
+    
+    	
+}
