622aa866819f765f078330e5d9e13e316edc2f5a
Ta180m <ta180m@gmail.com>
2019-08-16 20:02:15 -0500

    Update template.cpp

--- Miscellaneous/template.cpp
+++ Miscellaneous/template.cpp
@@ -10,12 +10,11 @@
 #include <unordered_map>
 #include <cmath>
 #include <cstring>
-#define pname (string)""
-#define init_io ifstream cin(pname+".in"); ofstream cout(pname+".out"); ios_base::sync_with_stdio(false); cin.tie(NULL)
-#define FOR(i, a, b) for (int i = (a); i < (b); i++)
-#define F0R(i, a) for (int i = 0; i < (a); i++)
-#define FORd(i, a, b) for (int i = (b) - 1; i >= (a); i--)
-#define F0Rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define init_io(pname) ifstream cin((string)pname+".in"); ofstream cout((string)pname+".out"); ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define FOR(i, a, b, in) for (int i = (a); i < (b); i += in)
+#define REP(i, a, b) for (int i = (a); i < (b); i++)
+#define RFOR(i, a, b, in) for (int i = (a) - 1; i >= (b); i -= in)
+#define RREP(i, a, b) for (int i = (a) - 1; i >= (b); i--)
 #define trav(a, x) for (auto& a : x)
 #define mp make_pair
 #define pb push_back
@@ -26,6 +25,7 @@
 #define sz(x) (int)x.size()
 #define all(x) begin(x), end(x)
 #define rsz resize
+#define mem(a, b) memset(a, (b), sizeof(a))
 using namespace std;
 typedef long long ll;
 typedef long double ld;
@@ -36,7 +36,7 @@ constexpr auto INF = (int)1e9;
 constexpr auto LINF = (ll)1e18;
 
 int main() {
-    init_io;
+    init_io("abc");
 
 
     
