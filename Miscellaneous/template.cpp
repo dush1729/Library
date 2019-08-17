d9d55101a1792ea633b467ddfebb3b95fad8c952
Ta180m <ta180m@gmail.com>
2019-08-16 19:48:21 -0500

    Update template.cpp

--- Miscellaneous/template.cpp
+++ Miscellaneous/template.cpp
@@ -10,17 +10,34 @@
 #include <unordered_map>
 #include <cmath>
 #include <cstring>
+#define pname (string)""
+#define init_io ifstream cin(pname+".in"); ofstream cout(pname+".out"); ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b) - 1; i >= (a); i--)
+#define F0Rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+#define rsz resize
 using namespace std;
 typedef long long ll;
-typedef pair<int, int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
+typedef long double ld;
+typedef pair<int, int> ii, typedef pair<ll, ll> pl, typedef pair<ld, ld> pd;
+typedef vector<int> vi, typedef vector<ll> vl, typedef vector<ld> vd;
+typedef vector<ii> vii, typedef vector<pl> vpl, typedef vector<pd> vpd;
 constexpr auto INF = (int)1e9;
+constexpr auto LINF = (ll)1e18;
 
 int main() {
-	ifstream cin(".in");
-    ofstream cout(".out");
+    init_io;
+
 
     
-    	
 }
