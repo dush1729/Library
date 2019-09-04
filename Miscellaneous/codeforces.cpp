2d0fce37fcd2d0af81cd5dc005e67ca1cead95bd
Ta180m <ta180m@gmail.com>
2019-09-04 13:51:14 -0500

    Create codeforces.cpp
    Add Codeforces template

---
+++ Miscellaneous/codeforces.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define init_io ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define FOR(i, a, b, in) for (int i = (a); i < (b); i += in)
+#define REP(i, a, b) for (int i = (a); i < (b); i++)
+#define RFOR(i, a, b, in) for (int i = (a) - 1; i >= (b); i -= in)
+#define RREP(i, a, b) for (int i = (a) - 1; i >= (b); i--)
+#define trav(a, x) for (auto& a : x)
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+#define mem(a, b) memset(a, (b), sizeof(a))
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii, typedef pair<ll, ll> pl, typedef pair<ld, ld> pd;
+typedef vector<int> vi, typedef vector<ll> vl, typedef vector<ld> vd;
+typedef vector<ii> vii, typedef vector<pl> vpl, typedef vector<pd> vpd;
+constexpr auto INF = (int)1e9;
+constexpr auto LINF = (ll)1e18;
+
+int main() {
+    init_io;
+
+
+    
+}
