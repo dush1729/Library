3a066c6813cc6bb5012cd7fe60b9be347333db39
Ta180m <ta180m@gmail.com>
2020-01-17 14:33:55 -0600

    update util.cpp

--- Miscellaneous/util.cpp
+++ Miscellaneous/util.cpp
@@ -3,5 +3,5 @@ void compress(BidirectionalIterator first, BidirectionalIterator last) {
 	vector<auto> tmp(first, last);
 	sort(tmp.begin(), tmp.end());
 	tmp.resize(unique(tmp.begin(), tmp.end()) - tmp.begin());
-	for (auto it = first; it != last; it++)* it = lower_bound(tmp.begin(), tmp.end(), *it) - tmp.begin();
+	for (auto it = first; it != last; it++) *it = lower_bound(tmp.begin(), tmp.end(), *it) - tmp.begin();
 }
