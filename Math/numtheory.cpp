b7c7075fe649ea7494920aa94d07fa9a76e2d0cd
Ta180m <ta180m@gmail.com>
2020-04-10 17:50:52 -0500

    Update numtheory.cpp

--- Math/numtheory.cpp
+++ Math/numtheory.cpp
@@ -1,13 +1,13 @@
 constexpr ll MOD = 1e9+7;
 
-int pow(int base, int exp) {
+int pw(int base, int exp) {
 	int res = 1;
 	while (exp) {
-		if (exp & 1) res = ((ll)base * res) % MOD;
+		if (exp & 1) res = (ll)base * res % MOD;
 		exp >>= 1;
-		base = ((ll)base * base) % MOD;
+		base = (ll)base * base % MOD;
 	}
 	return res;
 }
 
-int inv(int x) { return pow(x, MOD - 2); }
+int inv(int x) { return pw(x, MOD - 2); }
