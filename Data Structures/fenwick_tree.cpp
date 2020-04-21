dbf633771d50d451286be19fd9cef41d04839ef4
Ta180m <ta180m@gmail.com>
2020-04-20 22:22:35 -0500

    update fenwick tree

--- Data Structures/fenwick_tree.cpp
+++ Data Structures/fenwick_tree.cpp
@@ -4,5 +4,5 @@ public:
 	fenwick_tree(int N) { FT.assign(N + 1, 0); }
 	void update(int x, T val) { if (x > 0) for (; x < FT.size(); x += x & -x) FT[x] += val; }
 	T query(int x) { T ret = 0; if (x > 0) for (; x > 0; x -= x & -x) ret += FT[x]; return ret; }
-	T query(int x, int y) { return query(y) - (x == 1 ? 0 : query(x - 1)); }
+	T query(int x, int y) { return query(y) - query(x - 1); }
 };
