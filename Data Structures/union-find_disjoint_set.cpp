c3fc349763705d1d9e4032e34b5fdc72893787fa
Ta180m <ta180m@gmail.com>
2020-04-24 16:39:57 -0500

    Update union-find_disjoint_set.cpp

--- Data Structures/union-find_disjoint_set.cpp
+++ Data Structures/union-find_disjoint_set.cpp
@@ -2,9 +2,9 @@ class UFDS {
 private: vector<int> p, rank;
 public:
 	UFDS(int N) {
-		p.assign(N, 0);
-		for (int i = 0; i < N; i++) p[i] = i;
-		rank.assign(N, 0);
+		p.assign(N + 1, 0);
+		for (int i = 0; i <= N; i++) p[i] = i;
+		rank.assign(N + 1, 0);
 	}
 	int find_set(int i) { return (p[i] == i) ? i : (p[i] = find_set(p[i])); }
 	bool same_set(int i, int j) { return find_set(i) == find_set(j); }
