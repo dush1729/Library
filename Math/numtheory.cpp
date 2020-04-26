beacea7a524b34b9c5afe3b66bf4a1bf60fed147
Ta180m <ta180m@gmail.com>
2020-04-26 17:03:23 -0500

    Update numtheory.cpp

--- Math/numtheory.cpp
+++ Math/numtheory.cpp
@@ -1,5 +1,7 @@
 constexpr ll MOD = 1e9+7;
 
+int fact[200002] = { 1 }, ifact[200002] = { 1 };
+
 int pw(int base, int exp) {
 	int res = 1;
 	while (exp) {
@@ -11,3 +13,10 @@ int pw(int base, int exp) {
 }
 
 int inv(int x) { return pw(x, MOD - 2); }
+
+int nCr(int n, int k) { return (ll)fact[n] * ifact[k] % MOD * ifact[n - k] % MOD; }
+
+for (int i = 0; i < n; ++i) {
+	fact[i + 1] = (i + 1ll) * fact[i] % MOD;
+	ifact[i + 1] = inv(fact[i + 1]);
+}
