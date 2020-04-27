5faf4e7142044c5368549dee279bf0e3b2fd6a67
Ta180m <ta180m@gmail.com>
2020-04-27 13:03:28 -0500

    Update segment_tree_v3.cpp

--- Data Structures/segment_tree_v3.cpp
+++ Data Structures/segment_tree_v3.cpp
@@ -1,5 +1,5 @@
 int seg[400004], tmp[400004];
-inline void pull(int n) { seg[n] = seg[n << 1] + seg[n << 1 | 1]; }
+inline int pull(const int& l, const int& r) { return l + r; }
 inline void push(int l, int r, int n) {
 	seg[n] += (r - l + 1) * tmp[n];
 	if (l != r) tmp[n << 1] += tmp[n], tmp[n << 1 | 1] += tmp[n];
@@ -16,7 +16,7 @@ void update(int a, int b, int v, int l = 0, int r = -1, int n = 1) {
 	else {
 		int m = (l + r) >> 1;
 		update(a, b, v, l, m, n << 1), update(a, b, v, m + 1, r, n << 1 | 1);
-		pull(n);
+		seg[n] = pull(seg[n << 1], seg[n << 1 | 1]);
 	}
 }
 int query(int a, int b, int l = 0, int r = -1, int n = 1) {
@@ -25,5 +25,5 @@ int query(int a, int b, int l = 0, int r = -1, int n = 1) {
 	if (tmp[n]) push(l, r, n);
 	if (l >= a && r <= b) return seg[n];
 	int m = (l + r) >> 1;
-	return query(a, b, l, m, n << 1) + query(a, b, m + 1, r, n << 1 | 1);
+	return pull(query(a, b, l, m, n << 1), query(a, b, m + 1, r, n << 1 | 1));
 }
