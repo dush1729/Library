703363d5abfe07cf43533b37be625832df9180da
Ta180m <ta180m@gmail.com>
2020-05-19 21:23:51 -0500

    Update points_lines_vectors.cpp

--- Geometry/points_lines_vectors.cpp
+++ Geometry/points_lines_vectors.cpp
@@ -1,9 +1,9 @@
 constexpr auto EPS = numeric_limits<double>::epsilon();
 
 struct point {
-	int x, y;
+	ll x, y;
 	point() { x = y = 0; }
-	point(int _x, int _y) : x(_x), y(_y) {}
+	point(ll _x, ll _y) : x(_x), y(_y) {}
 	bool operator < (point p) const { return (x == p.x && y < p.y) || x < p.x; }
 	bool operator == (point p) const { return x == p.x && y == p.y; }
 };
@@ -11,51 +11,46 @@ struct point {
 double dist(point& p1, point& p2) { return hypot(p1.x - p2.x, p1.y - p2.y); }
 
 struct vec {
-	int x, y;
-	vec(int _x, int _y) : x(_x), y(_y) {}
-	vec(point a, point b) { return vec(b.x - a.x, b.y - a.y); }
+	ll x, y;
+	vec(ll _x, ll _y) : x(_x), y(_y) {}
+	vec(point a, point b) { x = b.x - a.x, y = b.y - a.y; }
 };
 
-ll dot(vec a, vec b) { return (ll)a.x * b.x + (ll)a.y * b.y; }
+ll dot(vec a, vec b) { return a.x * b.x + a.y * b.y; }
 
-ll norm_sq(vec v) { return (ll)v.x * v.x + (ll)v.y * v.y; }
+ll norm_sq(vec v) { return v.x * v.x + v.y * v.y; }
 
-ll cross(vec a, vec b) { return (ll)a.x * b.y - (ll)a.y * b.x; }
+ll cross(vec a, vec b) { return a.x * b.y - a.y * b.x; }
 
 double angle(point a, point o, point b) {
-	vec oa = to_vec(o, a), ob = to_vec(o, b);
+	vec oa(o, a), ob(o, b);
 	return acos(dot(oa, ob) / sqrt(norm_sq(oa) * norm_sq(ob)));
 }
 
-bool ccw(point p, point q, point r) { return cross(to_vec(p, q), to_vec(p, r)) > 0; }
-
-double angle(point a, point o, point b) {
-	vec oa = to_vec(o, a), ob = to_vec(o, b);
-	return acos(dot(oa, ob) / (sqrt(norm_sq(oa)) * sqrt(norm_sq(ob))));
-}
+bool ccw(point p, point q, point r) { return cross(vec(p, q), vec(p, r)) > 0; }
 
 struct line {
 	ll a, b, c;
 	bool operator < (const line& l) const { return tie(a, b, c) < tie(l.a, l.b, l.c); }
 	line(point p1, point p2) {
-	    ll a = p2.y - p1.y, b = p1.x - p2.x, c = p1.y * p2.x - p1.x * p2.y;
+	    a = p2.y - p1.y, b = p1.x - p2.x, c = p1.y * p2.x - p1.x * p2.y;
 	    ll d = __gcd(__gcd(abs(a), abs(b)), abs(c));
-	    return { a / d, b / d, c / d };
+	    a /= d, b /= d, c /= d;
 	}
 };
 
 double point_to_line(point p, line l) { return abs(l.a * p.x + l.b * p.y + l.c) / hypot(l.a, l.b); }
 
 line perp_bi(point p1, point p2) {
-	int a = 2 * (p2.x - p1.x), b = 2 * (p2.y - p1.y), c = -a * (p1.x + p2.x) / 2 - b * (p1.y + p2.y) / 2;
-	int d = gcd(gcd(abs(a), abs(b)), abs(c));
+	ll a = 2 * (p2.x - p1.x), b = 2 * (p2.y - p1.y), c = -a * (p1.x + p2.x) / 2 - b * (p1.y + p2.y) / 2;
+	ll d = __gcd(__gcd(abs(a), abs(b)), abs(c));
 	return { a / d, b / d, c / d };
 }
 
 point reflect(point p, line l) {
-	int x = p.x * (l.b * l.b - l.a * l.a) - 2 * l.a * (l.b * p.y + l.c);
-	int y = p.y * (l.a * l.a - l.b * l.b) - 2 * l.b * (l.a * p.x + l.c);
-	int d = l.a * l.a + l.b * l.b;
+	ll x = p.x * (l.b * l.b - l.a * l.a) - 2 * l.a * (l.b * p.y + l.c);
+	ll y = p.y * (l.a * l.a - l.b * l.b) - 2 * l.b * (l.a * p.x + l.c);
+	ll d = l.a * l.a + l.b * l.b;
 	if (x % d && y % d) return { 10001, 10001 };
 	else return { x / d, y / d };
 }
@@ -65,10 +60,10 @@ struct seg {
 	seg(point _p1, point _p2) : p1(_p1), p2(_p2) {}
 };
 
-line::line(seg s) { return to_line(s.p1, s.p2); }
+line::line(seg s) { line(s.p1, s.p2); }
 
 double point_to_seg(point p, seg s) {
-	int a = norm_sq(to_vec(s.p1, s.p2)), b = norm_sq(to_vec(p, s.p1)), c = norm_sq(to_vec(p, s.p2));
+	ll a = norm_sq(to_vec(s.p1, s.p2)), b = norm_sq(to_vec(p, s.p1)), c = norm_sq(to_vec(p, s.p2));
 	if (a + b > c && a + c > b) return point_to_line(p, to_line(s));
 	else return min(dist(p, s.p1), dist(p, s.p2));
 }
