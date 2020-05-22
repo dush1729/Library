f7b0245a50fc7ded759c55c81e433697efa1ff27
Anthony Wang <ta180m@gmail.com>
2020-05-22 17:31:05 -0500

    Update basic.cpp

--- Template/basic.cpp
+++ Template/basic.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
+#define f first
+#define s second
 using namespace std;
 typedef long long ll;
+typedef pair<int, int> ii;
 
 int main() {
+    // if (fopen("in", "r")) freopen("in", "r", stdin), freopen("out", "w", stdout);
     
 }
