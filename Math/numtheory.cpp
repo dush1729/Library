f25ac89ddafe14868683361bc1861aaa1fc116ff
Ta180m <ta180m@gmail.com>
2020-06-15 10:26:56 -0500

    update

--- Math/numtheory.cpp
+++ Math/numtheory.cpp
@@ -6,8 +6,7 @@ int pw(int base, int exp) {
 	int res = 1;
 	while (exp) {
 		if (exp & 1) res = (ll)base * res % MOD;
-		exp >>= 1;
-		base = (ll)base * base % MOD;
+		exp >>= 1, base = (ll)base * base % MOD;
 	}
 	return res;
 }
--- Template/basic.cpp
+++ Template/basic.cpp
@@ -2,10 +2,12 @@
 #define f first
 #define s second
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> ii;
+using ll = long long;
+using ii = pair<int, int>;
 
 int main() {
     // if (fopen("in", "r")) freopen("in", "r", stdin), freopen("out", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    
     
 }
