9094eb40f6ab4785035c8e438d49b1ba8bdf5959
Ta180m <ta180m@gmail.com>
2020-06-18 12:10:21 -0500

    Update basic.cpp

--- Template/basic.cpp
+++ Template/basic.cpp
@@ -6,8 +6,7 @@ using ll = long long;
 using ii = pair<int, int>;
 
 int main() {
-    // if (fopen("in", "r")) freopen("in", "r", stdin), freopen("out", "w", stdout);
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     
     
 }
