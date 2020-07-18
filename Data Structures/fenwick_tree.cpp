c7bf1734730ba95155a30457d735e4a0282e68a3
Anthony Wang <ta180m@gmail.com>
2020-07-17 23:02:06 -0500

    Update fenwick_tree.cpp

--- Data Structures/fenwick_tree.cpp
+++ Data Structures/fenwick_tree.cpp
@@ -1,8 +1,8 @@
 template<typename T> class fenwick_tree {
 private: vector<T> FT;
 public:
-	fenwick_tree(int N) { FT.assign(N + 1, 0); }
-	void update(int x, T val) { if (x > 0) for (; x < FT.size(); x += x & -x) FT[x] += val; }
-	T query(int x) { T ret = 0; if (x > 0) for (; x > 0; x -= x & -x) ret += FT[x]; return ret; }
+	fenwick_tree(int N) { FT.assign(N + 5, 0); }
+	void update(int x, T val) { if (++x) for (; x < FT.size(); x += x & -x) FT[x] += val; }
+	T query(int x) { T ret = 0; if (++x) for (; x > 0; x -= x & -x) ret += FT[x]; return ret; }
 	T query(int x, int y) { return query(y) - query(x - 1); }
 };
