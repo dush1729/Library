e0526ad005a6e7aa40290e93ce5c1f320bb97a0b
Anthony Wang <ta180m@gmail.com>
2020-08-24 09:41:18 -0500

    Update numtheory.cpp

--- Math/numtheory.cpp
+++ Math/numtheory.cpp
@@ -11,11 +11,8 @@ inline ll pw(ll base, ll exp) {
 	return res;
 }
 
-inline ll inv(ll x) { return pw(x, MOD - 2); }
+inline ll inv(ll x) { return pw(x, MOD-2); }
 
-inline ll nCr(int n, int k) { return fact[n] * ifact[k] % MOD * ifact[n - k] % MOD; }
+inline ll nCr(int n, int k) { return fact[n]*ifact[k]%MOD*ifact[n-k]%MOD; }
 
-for (int i = 0; i < N; ++i) {
-	fact[i + 1] = (i + 1ll) * fact[i] % MOD;
-	ifact[i + 1] = inv(fact[i + 1]);
-}
+for (int i = 0; i < N; ++i) fact[i+1] = (i+1ll)*fact[i]%MOD, ifact[i+1] = inv(fact[i+1]);
