dcb2b2d3768f6467e68d285b48525ece51fd13ac
Anthony Wang <ta180m@gmail.com>
2020-09-06 10:59:36 -0500

    Fixed a few things in lca.cpp

--- Graph/lca.cpp
+++ Graph/lca.cpp
@@ -7,12 +7,12 @@ namespace lca {
     }
     int lca(vector<int> * G, int u, int v) {
         if (d[u] > d[v]) swap(u, v);
-        for (int i = 18; i >= 0; i--) if (d[v] - (1 << i) >= d[u]) v = L[v][i];
+        for (int i = 18; i >= 0; --i) if (d[v] - (1 << i) >= d[u]) v = L[v][i];
         if (u == v) return u;
-        for (int i = 18; i >= 0; i--) if (L[u][i] && L[u][i] != L[v][i]) u = L[u][i], v = L[v][i];
+        for (int i = 18; i >= 0; --i) if (L[u][i] && L[u][i] != L[v][i]) u = L[u][i], v = L[v][i];
         return L[u][0];
     }
 	inline int dist(vector<int> * G, int u, int v) {
-		return d[u]+d[v]-2*lca(G, u, v);
+		return d[u]+d[v]-2*d[lca(G, u, v)];
 	}
 }
