6087e1adebc9025681c0efbaa59d530472af0d2f
Anthony Wang <ta180m@gmail.com>
2021-01-19 21:44:08 -0600

    Update fenwick_tree.cpp

--- Data Structures/fenwick_tree.cpp
+++ Data Structures/fenwick_tree.cpp
@@ -1,7 +1,7 @@
 template<typename T> class fenwick_tree {
 private: int N; T FT[MX];
 public:
-	fenwick_tree(int n) { N = n }
+	fenwick_tree(int n) { N = n; }
 	fenwick_tree(int n, T A[]) { N = n; memcpy(FT, A, sizeof FT); }
 	void update(int x, T val) { if (++x) for (; x < N; x += x&-x) FT[x] += val; }
 	T query(int x) { T ret = 0; if (++x) for (; x; x -= x&-x) ret += FT[x]; return ret; }
