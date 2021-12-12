7761f38957fecd5f45bc62d55e9a77636a2b11d5
Dushyant Singh <dush1729@gmail.com>
2021-12-12 17:22:34 +0530

    fix off by one in sparse table
    Given code fails for `query(0, n - 1)`
    Problem to test on https://codeforces.com/contest/1549/problem/D

--- Data Structures/sparse_table.cpp
+++ Data Structures/sparse_table.cpp
@@ -8,7 +8,7 @@ public:
 		for (int i = 0; i <= log(N); i++) st[i].resize(N);
 		for (int i = 0; i < N; i++) st[0][i] = A[i];
 		for (int i = 1; i <= log(N); i++) {
-			for (int j = 0; j + (1 << i) < N; j++) st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+			for (int j = 0; j + (1 << i) <= N; j++) st[i][j] = min(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
 		}
 	}
 
